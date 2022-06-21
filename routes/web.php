20b6b2b05d28ffb07ff1a647671bf64a9d32e694
interviewhubgit <interviewhubin@gmail.com>
2022-06-21 07:59:34 +0530

    categories management

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::resource('categories', CategoryController::class);
 Route::get('/', function () {
     return view('welcome');
 });
